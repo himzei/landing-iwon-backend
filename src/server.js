13851811c8c51d47d25e61216f354955edf79cdb
조현일 <himzei@gmail.com>
2023-02-19 08:23:34 +0900

    mongodb connect

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
+import "./db";
 import express from "express";
 import logger from "morgan";
+import userRouter from "./routers/userRouter";
 
 const app = express();
 
 app.use(logger("dev"));
+
+app.use("/api/v1/users", userRouter);
 
 const handleListening = () =>
   console.log(`Server listening on http://localhost:4000`);
