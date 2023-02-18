938989d20743ce70441fb7c956989b5ff7b357de
조현일 <himzei@gmail.com>
2023-02-18 22:40:28 +0900

    morgan middleware

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 import express from "express";
+import logger from "morgan";
 
 const app = express();
+
+app.use(logger("dev"));
 
 const handleListening = () =>
   console.log(`Server listening on http://localhost:4000`);
