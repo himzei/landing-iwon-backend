a6ded07465383419d10bf3dc7b813f61ef963dd6
조현일 <himzei@gmail.com>
2023-10-15 08:16:29 +0900

    university

--- src/controllers/consultingController.js
+++ src/controllers/consultingController.js
@@ -31,6 +31,51 @@ export const getList = async (req, res) => {
   }
 };
 
+export const postUniversity = async (req, res) => {
+  const { name, email, tel, category, message, type } = req.body;
+
+  if (name === "" || email === "" || tel === "") {
+    res.json({ ok: "false", error: "필수 입력사항을 작성하셔야 합니다. " });
+  }
+
+  const mailOptions = {
+    from: email,
+    to: ["himzei@gmail.com"],
+    subject: "[대구한의대]" + name + "님의 " + type + " 문의",
+    html: `
+  		<h1>${type}</h1>
+			<H3>이메일 : ${email}</h3>
+  		<h3>전화번호 : ${tel}</h3>
+  		<h3>관심분야 : ${category}</h3>
+  		<h3>전화번호 : ${tel}</h3>
+			<h3>문의내용</h3>
+			<div>
+			${message}
+			</div>
+
+  	`,
+    text: message,
+  };
+
+  await transporter.sendMail(mailOptions);
+
+  try {
+    await Consulting.create({
+      type,
+      name,
+      email,
+      tel,
+      category,
+      message,
+      createdAt: Date.now(),
+    });
+    res.json({ ok: "true" });
+  } catch (error) {
+    console.log(error);
+    res.json({ ok: "false", error: `에러가 발생했습니다. ${error.code}` });
+  }
+};
+
 export const postWrite = async (req, res) => {
   const { name, email, tel, category, message, type } = req.body;
 
@@ -57,10 +102,10 @@ export const postWrite = async (req, res) => {
     text: message,
   };
 
-  // 알림톡 시작
   const info = await transporter.sendMail(mailOptions);
   console.log(info);
 
+  // 알림톡 시작
   messageService.send({
     to: tel,
     from: "01071860119",
--- src/routers/consultingRouter.js
+++ src/routers/consultingRouter.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   getList,
+  postUniversity,
   postWrite,
   postWriteIcan,
 } from "../controllers/consultingController";
@@ -10,5 +11,6 @@ const consultingRouter = express.Router();
 consultingRouter.get("", getList);
 consultingRouter.post("/write", postWrite);
 consultingRouter.post("/writeIcan", postWriteIcan);
+consultingRouter.post("/writeUniversity", postUniversity);
 
 export default consultingRouter;
